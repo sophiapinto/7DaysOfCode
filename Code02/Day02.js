aedc6b8c32432d3183e7a868f987ae4b57338b07
Sophia Pinto <sarahsophiapinto@hotmail.com>
2022-04-02 21:00:05 -0300

    Update Day02

--- Code02/Day02.js
+++ Code02/Day02.js
@@ -25,3 +25,16 @@ const linguagem = prompt("Qual linguagem de programação você está estudando?
 const mensagem = "Olá, " + nome + " você tem " + idade + " anos e já está aprendendo " + linguagem +" !";
 
 alert(mensagem);
+
+const resposta = prompt("Você gosta de estudar " + linguagem + " ? Responda com o número 1 para SIM ou 2 para NÃO.");
+
+if (resposta == 1){
+    alert("Muito bom! Continue estudando e você terá muito sucesso.")
+}
+
+if (resposta == 2){
+    alert("Ahh que pena... Já tentou aprender outras linguagens?");
+}
+
+
+
